0b4061b964ca2c8e6bb4b5bf84fb636629716b50
freshdiefish <acs107125@gm.ntcu.edu.tw>
2019-05-05 03:53:57 +0800

    用parallax.js替換原視差滾動的方法

---
+++ main.js
@@ -0,0 +1,6 @@
+$("#head").parallax({ imageSrc: "./img/span/1.jpg" });
+$("#span1").parallax({ imageSrc: "./img/span/2.jpg" });
+$("#span2").parallax({ imageSrc: "./img/span/3.jpg" });
+$("#span3").parallax({ imageSrc: "./img/span/4.jpg" });
+$("#span4").parallax({ imageSrc: "./img/span/5.jpg" });
+$("#foot").parallax({ imageSrc: "./img/span/6.jpg" });
