f49851ac0772774bdfad5e5cc9d30dfa76fe118e
freshdiefish <acs107125@gm.ntcu.edu.tw>
2019-05-05 18:10:10 +0800

    加入fadeIn動畫

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+new WOW().init();
+$("h2").addClass("wow fadeInLeft");
+$(".wow").attr("data-wow-duration", "1s");
 $("#head").parallax({ imageSrc: "./img/span/1.jpg" });
 $("#span1").parallax({ imageSrc: "./img/span/2.jpg" });
 $("#span2").parallax({ imageSrc: "./img/span/3.jpg" });
